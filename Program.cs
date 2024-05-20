50faeb6da2a8f5f090f67338914e79006de7f71a
danielmichaneck <daniel.michaneck@gmail.com>
2024-05-20 15:12:18 +0200

    Updated methods.

--- Program.cs
+++ Program.cs
@@ -1,36 +1,69 @@
-﻿namespace Personalregister
+﻿// Program för att lägga till och visa personer i ett personalregister.
+// Registret sparas ej.
+// Programmet förutsätter korrekt inmatning; det kan inte hantera exceptions.
+// Programmet är "skrivet på svenska" för att göra det lättförståeligt.
+
+// Uppgift 1: 
+
+// Uppgift 2: 
+
+// 
+
+namespace Personalregister
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vänligen välj ett av följande alternativ:\n" +
-                "0) Lägg till en person i personalregistret.\n" +
-                "1) Vissa personalen i registret i en lista.");
+            bool igång = true;
 
-            switch(int.Parse(Console.ReadLine()))
+            Register personalRegister = new Register();
+
+            while (igång)
             {
-                case 0:
+                    Console.WriteLine("Vänligen välj ett av följande alternativ:\n" +
+                    "0) Lägg till en person i personalregistret.\n" +
+                    "1) Vissa personalen i registret i en lista.\n" +
+                    "2) Avsluta programmet.\n");
 
-                    break;
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 0:
+                        Console.WriteLine("\nDu har valt att lägga till en person i personalregistret." +
+                            "Vänligen skriv in namnet på personen.");
+                        string namn = Console.ReadLine();
+                        Console.WriteLine("Skriv in personens lön");
+                        int lön = int.Parse(Console.ReadLine());
+                        personalRegister.LäggTillPersonal(new Personal(namn, lön));
+                        Console.WriteLine("{0} med {1:C} i lön har lagts till i personalregistret.", namn, lön);
+                        break;
 
-                case 1:
+                    case 1:
+                        Console.WriteLine();
+                        personalRegister.SkrivListaÖverPersonalen();
+                        break;
 
-                    break;
+                    case 2:
+                        igång = false;
+                        break;
 
-                default:
+                    default:
 
-                    break;
+                        break;
+                }
+
+                // Skapar mellanrum till nästa iteration.
+                Console.WriteLine();
             }
         }
     }
 
     internal class Personal
     {
-        string namn;
-        int lön;
+        public string namn { get; set; }
+        public int lön {  get; set; }
 
-        Personal(string n, int l)
+        public Personal(string n, int l)
         {
             namn = n;
             lön = l;
@@ -50,7 +83,8 @@
         {
             foreach (Personal person in register)
             {
-                Console.WriteLine("");
+                Console.WriteLine("{0}  {1:C}", person.namn, person.lön);
+            }
         }
     }
 }
